7a805029924949819c89a2cac7a0c156f0c6a62e
Lunjie Li <lunjielee@qq.com>
2022-04-04 17:24:34 -0400

    Test LCD text output

--- handler.c
+++ handler.c
@@ -1,15 +1,21 @@
+#include "address_map_arm.h"
+#include "lcd_driver.h"
+#include "lcd_graphic.h"
+
 #define GPIO_BASE 0xFF200060
 #define HEX3_HEX0_BASE 0XFF200020
 #define HEX5_HEX4_BASE 0XFF200030
 #define HERTZ 1000000 // the timer is 200MHZ but it's too slow
 #define SEVEN_SEGMENT_DISPLAY_BASE 0xFF200020
 
-
 volatile unsigned int *const hex3_hex0_ptr = (unsigned int *)HEX3_HEX0_BASE;
 volatile unsigned int *const hex5_hex4_ptr = (unsigned int *)HEX5_HEX4_BASE;
 volatile unsigned int *const gpio_ptr = (unsigned int *)GPIO_BASE;
 volatile unsigned int *const seven_segment_display_ptr = (unsigned int *)SEVEN_SEGMENT_DISPLAY_BASE;
 volatile unsigned int *const seven_segment_display_ptr = (unsigned int *)SEVEN_SEGMENT_DISPLAY_BASE;
+
+char lights_on[13] = "Lights On \0";
+char lights_off[13] = "Lights Off\0";
 // timer////////////////////////////////////////////////////////////////
 
 typedef struct armTimer
@@ -62,12 +68,24 @@ int main(void)
 {
     *(gpio_ptr + 1) = 0; // enbale the input for GPIO
 
-    while(1){
-        if(*(gpio_ptr)){
+    // Initialize LCD21
+    init_spim0();
+    init_lcd();
+    clear_screen();
+
+    while (1)
+    {
+        if (*(gpio_ptr))
+        {
             lightDisplay(1);
+            LCD_text(lights_on, 0);
+            refresh_buffer();
         }
-        else{
+        else
+        {
             lightDisplay(0);
+            LCD_text(lights_off, 0);
+            refresh_buffer();
         }
     }
 }
